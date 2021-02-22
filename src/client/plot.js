0fa3e5fcc1bbbec83c83cd594402613e50edba4c
Aswanth KP <aswanth.kp@mountblue.tech>
2021-02-22 20:04:30 +0530

    Added src/client/index.html and src/client/plot.js to display bar chart of match played per year.

---
+++ src/client/plot.js
@@ -0,0 +1,73 @@
+ 
+
+
+const fetchMatchPlayedData = function (){
+    let matchPlayedArray =[];
+    fetch('http://localhost:3000/matchplayed').then(function(response) {
+        response.json().then(function(data) {
+            console.log(data)
+            console.log(' data loaded in api')
+            for(key in data){
+            matchPlayedArray.push([key,data[key]]);
+            }
+            displayMatchPlayedHighChart(matchPlayedArray);
+            });
+    });
+}
+
+
+function displayMatchPlayedHighChart(matchPlayedArray){
+    Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Matched Played per Year'
+    },
+    xAxis: {
+        type: 'category',
+        title: {
+            text: 'Year'
+            },
+        labels: {
+            rotation: -45,
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            },
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Match Played'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        pointFormat: 'Match Played : <b>{point.y:.1f} matches</b>'
+    },
+    series: [{
+        name: 'Match Played',
+        data: matchPlayedArray,
+        dataLabels: {
+            enabled: true,
+            rotation: -90,
+            color: '#FFFFFF',
+            align: 'right',
+            format: '{point.y:f}', // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    }]
+});
+}
+
+
+fetchMatchPlayedData();
+
